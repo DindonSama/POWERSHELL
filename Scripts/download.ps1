93508dc4f78c4cbe288765e932451d71915a8672
arthur <arthur@dindon.ovh>
2023-05-26 15:52:49 +0200

    -UseBasicParsing

--- Scripts/download.ps1
+++ Scripts/download.ps1
@@ -3,7 +3,7 @@ $CONF = @('General')
 
 [Net.ServicePointManager]::SecurityProtocol = [net.SecurityProtocolType]::Tls12
 
-$response = Invoke-WebRequest -Uri "https://api.github.com/repos/DindonSama/POWERSHELL/commits/main"
+$response = Invoke-WebRequest -UseBasicParsing -Uri "https://api.github.com/repos/DindonSama/POWERSHELL/commits/main"
 
 if ($response.statuscode -eq '200') {
 
@@ -11,14 +11,14 @@ if ($response.statuscode -eq '200') {
         New-Item -ItemType Directory -Force -Path "C:\Program Files\Zabbix Agent 2\scripts"
     }
     foreach ($LOOP_SCRIPT in $SCRIPT) {
-        Invoke-WebRequest "https://raw.githubusercontent.com/DindonSama/POWERSHELL/main/Scripts/$LOOP_SCRIPT.ps1" -OutFile "C:\Program Files\Zabbix Agent 2\scripts\$LOOP_SCRIPT.ps1"
+        Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/DindonSama/POWERSHELL/main/Scripts/$LOOP_SCRIPT.ps1" -OutFile "C:\Program Files\Zabbix Agent 2\scripts\$LOOP_SCRIPT.ps1"
     }
 
     if (!(Test-Path "C:\Program Files\Zabbix Agent 2\zabbix_agent2.d")) {
         New-Item -ItemType Directory -Force -Path "C:\Program Files\Zabbix Agent 2\zabbix_agent2.d"
     }
     foreach ($LOOP_CONF in $CONF) {
-        Invoke-WebRequest "https://raw.githubusercontent.com/DindonSama/POWERSHELL/main/zabbix_agent2.d/$LOOP_CONF.conf" -OutFile "C:\Program Files\Zabbix Agent 2\zabbix_agent2.d\$LOOP_CONF.conf"
+        Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/DindonSama/POWERSHELL/main/zabbix_agent2.d/$LOOP_CONF.conf" -OutFile "C:\Program Files\Zabbix Agent 2\zabbix_agent2.d\$LOOP_CONF.conf"
     }
 
     $keyValue= ConvertFrom-Json $response.Content | Select-Object -expand "sha"
